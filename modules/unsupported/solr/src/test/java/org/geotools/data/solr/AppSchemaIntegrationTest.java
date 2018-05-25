e9f1fbd7c6b78fad4fbaaca5f904168b98838d49
Fernando Miño <fernandomino7@gmail.com>
2018-05-25 15:09:17 -0500

    Data models refactoring to own class files

--- modules/unsupported/solr/src/test/java/org/geotools/data/solr/AppSchemaIntegrationTest.java
+++ modules/unsupported/solr/src/test/java/org/geotools/data/solr/AppSchemaIntegrationTest.java
@@ -6,7 +6,6 @@ import com.vividsolutions.jts.geom.Point;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.Serializable;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,7 +13,6 @@ import java.util.List;
 import java.util.Map;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
@@ -22,11 +20,12 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.common.SolrInputDocument;
 import org.geotools.data.DataAccess;
 import org.geotools.data.DataAccessFinder;
 import org.geotools.data.FeatureSource;
 import org.geotools.data.complex.config.Types;
+import org.geotools.data.solr.SolrTypeData.SolrTypes;
+import org.geotools.data.solr.StationData.Stations;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
 import org.geotools.feature.NameImpl;
@@ -237,213 +236,5 @@ public final class AppSchemaIntegrationTest extends OnlineTestCase {
     @Override
     protected String getFixtureId() {
         return "appschema";
-    }
-
-    /** Station data model class */
-    @XmlRootElement
-    public static class StationData implements Serializable {
-        private String measurementName;
-        private String measurementTime;
-        private String measurementUnit;
-        private Integer measurementValue;
-        private String stationCode;
-        private Integer stationId;
-        private String stationLocation;
-        private String stationName;
-        private List<String> stationTag = new ArrayList<>();
-        private List<String> stationComment = new ArrayList<>();
-
-        public StationData() {}
-
-        public StationData(
-                String measurementName,
-                String measurementTime,
-                String measurementUnit,
-                Integer measurementValue,
-                String stationCode,
-                Integer stationId,
-                String stationLocation,
-                String stationName,
-                List<String> stationTag,
-                List<String> station_comment) {
-            super();
-            this.measurementName = measurementName;
-            this.measurementTime = measurementTime;
-            this.measurementUnit = measurementUnit;
-            this.measurementValue = measurementValue;
-            this.stationCode = stationCode;
-            this.stationId = stationId;
-            this.stationLocation = stationLocation;
-            this.stationName = stationName;
-            this.stationTag = stationTag;
-            this.stationComment = station_comment;
-        }
-
-        public SolrInputDocument toSolrDoc() {
-            SolrInputDocument result = new SolrInputDocument();
-            result.addField("measurement_name", this.measurementName);
-            result.addField("measurement_time", this.measurementTime);
-            result.addField("measurement_unit", this.measurementUnit);
-            result.addField("measurement_value", this.measurementValue);
-            result.addField("station_code", this.stationCode);
-            result.addField("station_id", this.stationId);
-            result.addField("station_location", this.stationLocation);
-            result.addField("station_name", this.stationName);
-            if (this.stationTag != null) {
-                for (String str1 : this.stationTag) {
-                    result.addField("station_tag", str1);
-                }
-            }
-            if (this.stationComment != null) {
-                for (String str1 : this.stationComment) {
-                    result.addField("station_comment", str1);
-                }
-            }
-
-            return result;
-        }
-
-        public String getMeasurementName() {
-            return measurementName;
-        }
-
-        public void setMeasurementName(String measurementName) {
-            this.measurementName = measurementName;
-        }
-
-        public String getMeasurementTime() {
-            return measurementTime;
-        }
-
-        public void setMeasurementTime(String measurementTime) {
-            this.measurementTime = measurementTime;
-        }
-
-        public String getMeasurementUnit() {
-            return measurementUnit;
-        }
-
-        public void setMeasurementUnit(String measurementUnit) {
-            this.measurementUnit = measurementUnit;
-        }
-
-        public Integer getMeasurementValue() {
-            return measurementValue;
-        }
-
-        public void setMeasurementValue(Integer measurementValue) {
-            this.measurementValue = measurementValue;
-        }
-
-        public String getStationCode() {
-            return stationCode;
-        }
-
-        public void setStationCode(String stationCode) {
-            this.stationCode = stationCode;
-        }
-
-        public Integer getStationId() {
-            return stationId;
-        }
-
-        public void setStationId(Integer stationId) {
-            this.stationId = stationId;
-        }
-
-        public String getStationLocation() {
-            return stationLocation;
-        }
-
-        public void setStationLocation(String stationLocation) {
-            this.stationLocation = stationLocation;
-        }
-
-        public String getStationName() {
-            return stationName;
-        }
-
-        public void setStationName(String stationName) {
-            this.stationName = stationName;
-        }
-
-        public List<String> getStationTag() {
-            return stationTag;
-        }
-
-        public void setStationTag(List<String> stationTag) {
-            this.stationTag = stationTag;
-        }
-
-        public List<String> getStationComment() {
-            return stationComment;
-        }
-
-        public void setStationComment(List<String> stationComment) {
-            this.stationComment = stationComment;
-        }
-    }
-
-    @XmlRootElement
-    public static class Stations implements Serializable {
-        private List<StationData> stations = new ArrayList<>();
-
-        public Stations() {}
-
-        public List<StationData> getStations() {
-            return stations;
-        }
-
-        public void setStations(List<StationData> stations) {
-            this.stations = stations;
-        }
-    }
-
-    @XmlRootElement
-    public static class SolrTypes implements Serializable {
-        private List<SolrTypeData> types;
-
-        public SolrTypes() {}
-
-        public List<SolrTypeData> getTypes() {
-            return types;
-        }
-
-        public void setTypes(List<SolrTypeData> types) {
-            this.types = types;
-        }
-    }
-
-    @XmlRootElement
-    public static class SolrTypeData implements Serializable {
-        private String name;
-        private String type;
-        private Boolean multi;
-
-        public SolrTypeData() {}
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public Boolean getMulti() {
-            return multi;
-        }
-
-        public void setMulti(Boolean multi) {
-            this.multi = multi;
-        }
     }
 }
